a1cdee7ded911def60c0ca44e65759fd14820ae2
Aditya Rao (Tirumal Rao) <aditya.rao.in@gmail.com>
2018-06-20 12:10:40 +0530

    day 2 > video 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = "AIzaSyA0TQlZTYGGpB9uaYn_9muMmo2Zy5fSFPY";
 
-const App = function() {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        const state = {
+            videos: []
+        };
+
+        YTSearch({
+            key: API_KEY,
+            term: 'surfboards'
+        }, (videos) => {
+            this.setState({
+                videos
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
