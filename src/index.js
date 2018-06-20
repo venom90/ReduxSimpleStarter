03cbb55a6dd010fb36958a8b952edb15474d1151
Aditya Rao (Tirumal Rao) <aditya.rao.in@gmail.com>
2018-06-20 12:58:31 +0530

    day 2 > video 5

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyA0TQlZTYGGpB9uaYn_9muMmo2Zy5fSFPY";
 
@@ -28,6 +29,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
