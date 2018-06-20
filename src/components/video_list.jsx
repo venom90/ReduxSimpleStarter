fdb086c5178f70d76372ed6e7e0896b6d4bea708
Aditya Rao (Tirumal Rao) <aditya.rao.in@gmail.com>
2018-06-20 12:47:57 +0530

    day 2 > video 4

--- src/components/video_list.jsx
+++ src/components/video_list.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = ({videos}) => {
     if(!videos) {
         return <div>Loading...</div>;
     }
+
+    const videoItems = videos.map((video) => {
+        return <VideoListItem key={video.etag} video={video} />
+    });
+
     return (
         <ul className="col-md-4 list-group">
-            {videos.length}
+            {videoItems}
         </ul>
     );
 }
---
+++ src/components/video_list_item.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} className="media-object"/>
+            </div>
+
+            <div className="media-body">
+                <div className="media-heading">
+                    {video.snippet.title}
+                </div>
+            </div>
+        </li>
+    )
+};
+
+export default VideoListItem;
